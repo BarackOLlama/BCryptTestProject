247f9b246c72d82b128e0209bafef99e4f3518c3
gharamorth <mappelma@avans.nl>
2018-11-07 15:03:16 +0100

    Add project files.

---
+++ BCryptTest.Database/Account.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Account]
+(
+	[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
+    [username] VARCHAR(50) NOT NULL, 
+    [password] VARCHAR(64) NOT NULL, 
+    [salt] VARCHAR(32) NOT NULL
+)
---
+++ BCryptTestProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace BCryptTestProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string _username = "username";
+            string _password = "password";
+
+
+            using(var context = new BCryptTestEntities())
+            {
+                //add a new account to the db
+                //string salt = BCrypt.Net.BCrypt.GenerateSalt();
+                //string saltedpw = BCrypt.Net.BCrypt.HashPassword(_password, salt);
+                //context.Accounts.Add(new Account() {Id = 0, username = _username, password = saltedpw, salt = salt });
+                //context.SaveChanges();
+
+                //verifying a user in the database
+
+                var result = context.Accounts.First(e => e.username == _username);
+
+                if (result == null)
+                {
+                    Console.WriteLine("Invalid username/password combination");
+                    return;
+                }
+
+                var salt = result.salt;
+                var password = result.password;
+                var saltedpw = BCrypt.Net.BCrypt.HashPassword(_password, salt);
+
+                if (saltedpw.Equals(password))
+                {
+                    Console.WriteLine("Login succesful");
+                }else
+                {
+                    Console.WriteLine("Login failed");
+                }
+               
+            }
+            Console.WriteLine("Success?");
+        }
+    }
+}
